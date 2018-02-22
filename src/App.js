8dc81e10e1cad899516172b89aca2f389ee7bb56
Sam <skaram13st@gmail.com>
2018-02-22 18:49:45 -0500

    Fun Fieri Updates

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">WELCOME TO FLAVOR TOWN</h1>
+            <table className="App-table">
+                <tr className="App-row-default"><h2 className="App-fill" id="status" /></tr>
+                <tr className="App-row-default"><div className="fb-login-button App-fill" data-max-rows={1} data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto- logout-link="false" data-use-continue-as="false" /></tr>
+                <tr ><img src={fieri} className="App-logo" alt="logo" /></tr>
+            </table>
+            
         </header>
-      <body className="Flavor">
-      <img src={fieri} className="App-logo" alt="logo" />
-      </body>
       </div>
     );
   }
