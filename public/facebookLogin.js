f65a142927e87e8305deba9f8856f4f3dafe7023
Sam <skaram13st@gmail.com>
2018-02-26 11:40:40 -0500

    Nicely Formatted

--- public/facebookLogin.js
+++ public/facebookLogin.js
@@ -63,15 +63,26 @@
 
     // Here we run a very simple test of the Graph API after login is
     // successful.  See statusChangeCallback() for when this call is made.
+    //user_education_history,user_work_history
     function testAPI() {
         console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me?fields=id,email,first_name,last_name,user_birthday,user_education_history,user_work_history', function(response) {
-        document.getElementById('status').innerHTML =
-        'First Name: ' + response.first_name +
-            ' Last Name: ' + response.last_name +
-            ' Email: '+ response.email +
-            ' Birthday: ' + response.user_birthday + '!';
+        FB.api('/me?fields=id,email,first_name,last_name,gender', function(response) {
+        document.getElementById('first').innerHTML =
+        'First Name: ' + response.first_name;
+
+        document.getElementById('last').innerHTML =
+        'Last Name: ' + response.last_name;
+
+        document.getElementById('email').innerHTML =
+        'Email: ' + response.email;
+
+        document.getElementById('gender').innerHTML =
+        'Gender: ' + response.gender;
+
         });
+
+
+
     }
 
 
--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">WELCOME TO FLAVOR TOWN</h1>
             <table className="App-table">
-                <tr className="App-row-default"><h2 className="App-fill" id="status" /></tr>
-
                 <tr className="App-row-default"><div className="fb-login-button App-fill" scope="public_profile, user_birthday, user_education_history, user_work_history, email" data-max-rows={1} data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto- logout-link="true" data-use-continue-as="false" /></tr>
-                <tr ><img src={fieri} className="App-logo" alt="logo" /></tr>
+
+                <tr className="App-row-default"><h2 className="App-fill" id="first" /></tr>
+                <tr className="App-row-default"><h2 className="App-fill" id="last" /></tr>
+                <tr className="App-row-default"><h2 className="App-fill" id="email" /></tr>
+                <tr className="App-row-default"><h2 className="App-fill" id="gender" /></tr>
+
+
+
             </table>
             
         </header>
