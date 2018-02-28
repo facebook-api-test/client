784e12289140340e9625e95328fc0c4c0bbe29e1
Tiarnan Cooney <Tiarnan.Cooney@libertymutual.com>
2018-02-28 17:19:03 -0500

    Important - Added Homer

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import homer from '../../homer.gif';
 import '../../App.css';
 
 const Header = () =>
 {
     return (
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={homer} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
     );
